9ed5c3be67b3037b500c0cedeb0045cb0accbe51
nicholasadam84 <nicholasadam84@gmail.com>
2020-01-07 13:53:46 -0800

    fixed responsive design of ImageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,8 +8,8 @@ const ImageLinkForm = () => {
                 {'This App will detect faces in your pictures. Give it a try!'}
             </p>
             <div className="center">
-                <div className="form flex justify-center center pa4 br3 shadow-5 w-50">
-                    <input className="f4 pa2 w-70 center" type='text' />
+                <div className="form center pa4 br3 shadow-5">
+                    <input className="w-70 center f4 pa2" type='text' />
                     <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
 
                 </div>
