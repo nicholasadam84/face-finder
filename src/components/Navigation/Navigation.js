2ac6be13c2d80ffeb09f2c6b6cbf0c2e6bfad632
nicholasadam84 <nicholasadam84@gmail.com>
2020-01-06 16:39:05 -0800

    Created components ImageLinkForm, Logo, and Navigation and added some styling

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav className={`flex justify-end pa3`}>
+            <p className={`f3 link dim black underline pa3 pointer`}>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
