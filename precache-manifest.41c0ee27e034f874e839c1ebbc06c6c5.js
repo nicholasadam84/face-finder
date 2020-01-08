abdd51a2aeffbace1e75ba1d9690d59286effd37
nicholasadam84 <nicholasadam84@gmail.com>
2020-01-07 17:56:41 -0800

    Updates

---
+++ precache-manifest.41c0ee27e034f874e839c1ebbc06c6c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0750bf5324a20dab6f2fa826da4d46e1",
+    "url": "/face-finder/index.html"
+  },
+  {
+    "revision": "01492c1f1d93ef392dc1",
+    "url": "/face-finder/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6397f55fa5b1883463cf",
+    "url": "/face-finder/static/css/main.37991915.chunk.css"
+  },
+  {
+    "revision": "01492c1f1d93ef392dc1",
+    "url": "/face-finder/static/js/2.584f0f82.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-finder/static/js/2.584f0f82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6397f55fa5b1883463cf",
+    "url": "/face-finder/static/js/main.4bc50438.chunk.js"
+  },
+  {
+    "revision": "ae92bfa6cdf3bab83c5f",
+    "url": "/face-finder/static/js/runtime-main.9ab2b7b9.js"
+  }
+]);
