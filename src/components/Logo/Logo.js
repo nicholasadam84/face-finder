9e46e465d3902d17ad6d941f3915ece10c98d44e
nicholasadam84 <nicholasadam84@gmail.com>
2020-01-08 16:16:18 -0800

    removed LH

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner"> 
                     <img src={heart} alt="heart icon"/>
-                    <p>LooneyHealth</p> 
+                     
                 </div>
             </Tilt>
         </div>
