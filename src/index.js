a9cfef8656d10b6fa8fec86615d6eb8ec2019dd8
nicholasadam84 <nicholasadam84@gmail.com>
2020-02-20 16:54:57 -0800

    getting error for resolving tachyons, trying require() form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'tachyons';
+// import 'tachyons' from 'tachyons';
+const tachyons = require('tachyons');
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
